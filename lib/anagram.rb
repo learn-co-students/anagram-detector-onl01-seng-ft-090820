ef78944d98f63562527344fcc64c2ada3b72a023
Leah Schlackman <leahemily1@gmail.com>
2020-09-18 18:39:58 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+require 'pry' 
+
+class Anagram 
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end
+  # binding.pry
+  
+  def match(array)
+    new_arr = []
+    return_arr = []
+    # binding.pry
+    array.each do |item| 
+      new_arr = item.split("")
+     if new_arr.sort == @word.split("").sort
+       return_arr << item
+      end
+    end
+    return_arr
+  end
+  end
+   
+    
